9589dd411998ef74065a78860dcbfd4cabd2ce7a
bruninhogomez <gcbruno@iesrafaelalberti.es>
2025-05-08 19:06:21 +0200

    commit inicial

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,7 +6,6 @@ import es.prog2425.taskmanager.ui.ConsolaUI
 
 
 fun main(){
-
     val repositorio = ActividadRepositorioEnMemoria()
     val servicio = ActividadServicios(repositorio)
     val ui = ConsolaUI(servicio)
