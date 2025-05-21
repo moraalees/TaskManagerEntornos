a66978cbada6ac3288573d4fe01be324dbcb4538
cmorcan602 <mccristian@iesrafaelalberti.es>
2025-05-21 17:36:58 +0200

    ActividadRepositorioEnMemoria documentada

--- src/main/kotlin/data/ActividadRepositorioEnMemoria.kt
+++ src/main/kotlin/data/ActividadRepositorioEnMemoria.kt
@@ -3,13 +3,24 @@ package es.prog2425.taskmanager.data
 import es.prog2425.taskmanager.model.Actividad
 
 class ActividadRepositorioEnMemoria : IActividadRepository{
+    /**
+     * Lista mutable que almacena las actividades agregadas.
+     */
     private val actividades = mutableListOf<Actividad>()
 
-
+    /**
+     * Agrega una nueva [Actividad] al repositorio.
+     *
+     * @param actividad la actividad que se desea añadir.
+     */
     override fun agregarActividad(actividad: Actividad) {
         actividades.add(actividad)
     }
 
-
+    /**
+     * Retorna la lista de actividades almacenadas en memoria.
+     *
+     * @return lista de actividades.
+     */
     override fun obtenerActividades(): List<Actividad> = actividades
 }
